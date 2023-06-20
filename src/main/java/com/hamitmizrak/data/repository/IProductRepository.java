6d36cbb7ee5f59beeb498b0fd1a13c1a1767b871
hamitmizrak <hamitmizrak@gmail.com>
2023-06-20 11:40:16 +0300

    CommandLineRunner

--- src/main/java/com/hamitmizrak/data/repository/IProductRepository.java
+++ src/main/java/com/hamitmizrak/data/repository/IProductRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IProductRepository extends CrudRepository<ProductEntity,Long> {
+
+    // Delivered Query
+    ProductEntity findByProductName(String productName);
 }
